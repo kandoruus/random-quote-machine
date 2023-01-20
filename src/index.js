e06cd990a0a5c651b4c125a23a703d15426e7fdb
caleb <1d0nt3v3nl1ftbr0@gmail.com>
2023-01-20 11:19:55 -0500

    stylers

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import './index.css';
+/*import { Provider } from 'react-redux';*/
+import './styles.css';
 
 
 
-const container = document.getElementById('root');
+const container = document.getElementById('wrapper');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
   </React.StrictMode>
 );
-
