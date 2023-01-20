a3e6bf3852cfb76df7e671ad085b2a3419142e6e
caleb <1d0nt3v3nl1ftbr0@gmail.com>
2023-01-20 12:00:22 -0500

    sss

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import './index.css';
-/*
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
+//import { Provider } from 'react-redux';
 
+class NewQuoteButton extends React.Component {
+  render() {
+    return (
+      <button id="new-quote">New Quote</button>
+    );
+  }
+}
 
-
-const container = document.getElementById('wrapper');
+const container = document.getElementById('new-quote-container');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+    <NewQuoteButton />
   </React.StrictMode>
 );
-*/
