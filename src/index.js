465c59f3e4f549bd4b18802fe7562bc9979b9db3
caleb <1d0nt3v3nl1ftbr0@gmail.com>
2023-01-20 14:33:44 -0500

    done tests

--- src/index.js
+++ src/index.js
@@ -1,22 +1,57 @@
 import './index.css';
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-//import { Provider } from 'react-redux';
+import { quotes, colors } from './rand-arrays.js';
 
-class NewQuoteButton extends React.Component {
+const getRandomArrayElement = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+const twitterURLBase = 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=';
+
+
+class QuoteBox extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      themeColor: getRandomArrayElement(colors),
+      quoteData: getRandomArrayElement(quotes),
+    }
+    this.onNewQuoteClick = this.onNewQuoteClick.bind(this);
+    this.getTwitterURL = this.getTwitterURL.bind(this);
+  }
+
+  getTwitterURL() {
+    return twitterURLBase + encodeURI('"' + this.state.quoteData.quote + '" ' + this.state.quoteData.author);
+  }
+
+  onNewQuoteClick() {
+    this.setState({
+      themeColor: getRandomArrayElement(colors),
+      quoteData: getRandomArrayElement(quotes)
+    });
+  }
+
   render() {
     return (
-      <button id="new-quote">New Quote</button>
+      <div id="quote-box">
+        <div id="text">{this.state.quoteData.quote}</div>
+        <div id="author">{this.state.quoteData.author}</div>
+        <a id="tweet-quote" rel="noreferrer" target="_blank" href={this.getTwitterURL()}>
+          <i className="fa fa-twitter"></i>
+        </a>
+        <button id="new-quote" onClick={this.onNewQuoteClick}>New Quote</button>
+      </div>
     );
   }
 }
 
-const container = document.getElementById('new-quote-container');
+
+const container = document.getElementById('wrapper');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <NewQuoteButton />
+    <QuoteBox />
   </React.StrictMode>
 );
